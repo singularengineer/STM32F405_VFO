96ef55c4ac592eb132f590230cdc08c264da2a0c
Singular Engineer <singularengineer@outlook.com>
2020-04-26 18:19:10 -0400

    Initial commit

---
+++ Inc/App.h
@@ -0,0 +1,36 @@
+/*
+ * App.h
+ *
+ *  Created on: Sep 1, 2019
+ *      Author: Prem
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+volatile uint16_t ticker;
+volatile uint8_t dataAvailable;
+/* USER CODE END Private defines */
+
+ /* USER CODE BEGIN Prototypes */
+void MainApp();
+void Init_APP();
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_H_ */
---
+++ Src/App.c
@@ -0,0 +1,41 @@
+/*
+ * App.c
+ *
+ *  Created on: Sep 15, 2019
+ *      Author: Prem
+ */
+#include "App.h"
+#include "string.h"
+//#include "usbd_cdc_if.h"
+
+uint8_t tim4[] = "Tim 4: ";
+uint8_t newline[] = "\r\n";
+uint32_t encoder4 = 0;
+uint32_t encoder3 = 0;
+uint8_t usbbuff[12];
+
+void Init_APP()
+{
+	LL_SYSTICK_EnableIT();
+}
+
+void MainApp()
+{
+	if(ticker >= 100)
+	{
+		ticker = 0;
+		LL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+//		encoder4 = TIM4->CNT>>2;
+//		encoder3 = TIM3->CNT>>2;
+//		//CDC_Transmit_FS(usbstr,strlen((char *)usbstr));
+//		//CDC_Transmit_FS(usbstr,strlen((char *) itoa(encoder, usbbuff, 16)));
+//		//itoa(encoder4,usbbuff,10);
+//		itoa(encoder3,usbbuff,10);
+//
+//		while((CDC_Transmit_FS(tim4,strlen((char *)tim4)) == USBD_BUSY));
+//		while((CDC_Transmit_FS(usbbuff,strlen((char *)usbbuff)) == USBD_BUSY));
+//		while((CDC_Transmit_FS("\r\n",2) == USBD_BUSY));
+//		//CDC_Transmit_FS(newline,strlen((char *)newline));
+	}
+}
+
