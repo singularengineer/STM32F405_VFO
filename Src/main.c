751bc3e129dc2c22d0bd05a52fd3b95dc1976021
Singular Engineer <singularengineer@outlook.com>
2020-04-26 20:45:54 -0400

    PLL to 168MHz
    Increase the clock to 168MHz

--- Src/main.c
+++ Src/main.c
@@ -118,7 +118,7 @@ void SystemClock_Config(void)
 {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
 
-   if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5)
+  if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5)
   {
   Error_Handler();  
   }
@@ -130,7 +130,7 @@ void SystemClock_Config(void)
   {
     
   }
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 168, LL_RCC_PLLP_DIV_2);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 168, LL_RCC_PLLP_DIV_2);
   LL_RCC_PLL_Enable();
 
    /* Wait till PLL is ready */
@@ -148,8 +148,8 @@ void SystemClock_Config(void)
   {
   
   }
-  LL_Init1msTick(84000000);
-  LL_SetSystemCoreClock(84000000);
+  LL_Init1msTick(168000000);
+  LL_SetSystemCoreClock(168000000);
 }
 
 /**
